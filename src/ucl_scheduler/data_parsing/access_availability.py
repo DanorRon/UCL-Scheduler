f0f1eae92ce29171cd4df45c549398a4d3e5683c
Ronan Venkat <47954337+DanorRon@users.noreply.github.com>
2025-07-12 18:25:29 -0400

    Redo sheets processing for new format

--- src/ucl_scheduler/data_parsing/access_availability.py
+++ src/ucl_scheduler/data_parsing/access_availability.py
@@ -15,7 +15,7 @@ credentials_path = package_dir / "credentials" / "ucl-scheduler-866343adad65.jso
 gc = service_account(filename=credentials_path)
 
 # Open the spreadsheet by key
-spreadsheet = gc.open_by_key("1L18-4RtE4I8ku2kQcTl1HvfrzN0hZdSRFdrEI6XTJ4c")
+spreadsheet = gc.open_by_key("1gQgJI0Ar2K9pkKZkmZxzxPTR95355VsVShk5_aMvxIo")
 
 # Get worksheet data (assuming first worksheet)
 worksheet = spreadsheet.get_worksheet(0)
@@ -23,7 +23,3 @@ data = worksheet.get_all_values()
 
 # Convert to pandas DataFrame
 availability_df = pd.DataFrame(data)
-
-# Optionally, set the first row as header if appropriate
-availability_df.columns = availability_df.iloc[0]
-availability_df = availability_df[1:].reset_index(drop=True)
